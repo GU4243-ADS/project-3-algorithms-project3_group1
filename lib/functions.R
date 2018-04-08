f163297469a855b73c38ac8bd6cebcfbc05c313b
Juho Ma <35592552+juhoma@users.noreply.github.com>
2018-04-08 11:58:22 -0400

    Update functions.R

--- lib/functions.R
+++ lib/functions.R
@@ -105,7 +105,6 @@ calc_weight <- function(data, method = "pearson") {
         return(cosine(rowA[joint_values], rowB[joint_values]))
       }
       if (method == 'entropy')  {
-        library(entropy)
         rowA[rowA == 0] <- 0.000001
         rowB[rowB == 0] <- 0.000001
         return(KL.empirical(rowA,rowB))
