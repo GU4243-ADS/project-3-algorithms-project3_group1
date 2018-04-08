45f3188f0060e54580014be5a52ca7497801cae8
Juho Ma <35592552+juhoma@users.noreply.github.com>
2018-04-08 18:45:08 -0400

    Update memory_predict_script.R

--- doc/memory_predict_script.R
+++ doc/memory_predict_script.R
@@ -1,42 +1,43 @@
-# A simplification of memory_based_model.R
+######################################################
+### Memory-based Collaborative Filtering Algorithm ###
+######################################################
 
-# Path of functions.R
+### Project 3
+### ADS Spring 2018
+### Group 1
+
+### Set path for functions.R
+
 setwd("~/STAT4243/project-3-algorithms-project3_group1/lib")
 source("functions.R")
 
-### Builds UI Matrix for MS dataset
+### Build UI Matrix for MS dataset
 
-# Path of MS dataset
+# Set path for MS dataset
 setwd("~/STAT4243/project-3-algorithms-project3_group1/data/MS_sample")
 
 # Load the data
 MS_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
 MS_train <- MS_train[, 2:4]
 
-# Transform from narrow to wide, i.e. user-item matrix 
-# using MS_data_transform function
-
-# Below takes 2.17 minutes
+# Transform into UI matrix using MS_data_transform function
 MS_UI <- MS_data_transform(MS_train)
 save(MS_UI, file = "MS_UI.RData")
 
-### Build the UI Matrix for Movie Dataset
+### Build UI Matrix for Movie Dataset
 
-# Path of Movie Dataset
+# Set path for Movie Dataset
 setwd("~/STAT4243/project-3-algorithms-project3_group1/data/eachmovie_sample")
 
 # Load the data
 movie_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
 movie_train <- movie_train[, 2:4]
 
-# Compute the full matrix
-# Below takes about 4 minutes
-
+# Transform into UI matrix using movie_data_transform function
 movie_UI <- movie_data_transform(movie_train)
 save(movie_UI, file = "movie_UI.RData")
 
-# Calculate the full weights on the movie data
-# The below took 87 minutes on my Macbook, 35 on my iMac
+### Calculate the full weights on each data
 
 movie_sim <- calc_weight(movie_UI)
 save(movie_sim, file= "movie_sim.RData")
