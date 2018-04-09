36ce92d1e4e434f36c431c8691511973c96fccad
Juho Ma <35592552+juhoma@users.noreply.github.com>
2018-04-09 00:15:22 -0400

    Update memory_based.R

--- doc/memory_based.R
+++ doc/memory_based.R
@@ -87,21 +87,21 @@ save(MS_pred_simrank, file = "MS_pred_simrank.RData")
 
 ## Build UI Matrix
 
-# Set path for movie dataset
+# Set path for Movie Dataset
 setwd("~/STAT4243/project-3-algorithms-project3_group1/data/eachmovie_sample")
 
 # Load the data
-MS_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
-MS_train <- MS_train[, 2:4]
+movie_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
+movie_train <- movie_train[, 2:4]
 
-# Transform into UI matrix using MS_data_transform function
-MS_UI <- MS_data_transform(MS_train)
-save(MS_UI, file = "MS_UI.RData")
+# Transform into UI matrix using movie_data_transform function
+movie_UI <- movie_data_transform(movie_train)
+save(movie_UI, file = "movie_UI.RData")
 
 # Calculate similarity matrices using each measure
 setwd("~/STAT4243/project-3-algorithms-project3_group1/output")
 
-MS_sim_pearson <- calc_weight(MS_UI, method = "pearson")
+movie_sim_pearson <- calc_weight(MS_UI, method = "pearson")
 save(MS_sim_pearson, file= "MS_sim_pearson.RData")
 
 MS_sim_psig <- calc_weight(MS_UI, method = "psig")
@@ -149,49 +149,3 @@ save(MS_pred_pearson, file = "MS_pred_pearson.RData")
 
 MS_pred_simrank <- pred_matrix(MS_UI, MS_sim_simrank)
 save(MS_pred_simrank, file = "MS_pred_simrank.RData")
-
-
-
-#### Predict movie dataset
-
-### Build UI Matrix
-
-# Set path for Movie Dataset
-setwd("~/STAT4243/project-3-algorithms-project3_group1/data/eachmovie_sample")
-# Load the data
-movie_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
-movie_train <- movie_train[, 2:4]
-# Transform into UI matrix using movie_data_transform function
-movie_UI <- movie_data_transform(movie_train)
-save(movie_UI, file = "movie_UI.RData")
-
-### Calculate similarity matrices using each measure
-
-# Movie data
-movie_sim_pearson <- calc_weight(movie_UI, method = "pearson")
-save(movie_sim_pearson, file= "movie_sim_pearson.RData")
-
-movie_sim_psig <- calc_weight(movie_UI, method = "psig")
-save(movie_sim_psig, file= "movie_sim_psig.RData")
-
-movie_sim_pvar <- calc_weight(movie_UI, method = "pvar")
-save(movie_sim_pvar, file= "movie_sim_pvar.RData")
-
-movie_sim_spearman <- calc_weight(movie_UI, method = "spearman")
-save(movie_sim_spearman, file= "movie_sim_spearman.RData")
-
-movie_sim_vector <- calc_weight(movie_UI, method = "vector")
-save(movie_sim_vector, file= "movie_sim_vector.RData")
-
-movie_sim_entropy <- calc_weight(movie_UI, method = "entropy")
-save(movie_sim_entropy, file= "movie_sim_entropy.RData")
-
-movie_sim_msd <- calc_weight(movie_UI, method = "msd")
-save(movie_sim_msd, file= "movie_sim_msd.RData")
-
-movie_sim_simrank <- calc_weight(movie_UI, method = "simrank")
-save(movie_sim_simrank, file= "movie_sim_simrank.RData")
-
-# Calculate predictions for movies
-movie_pred <- pred_matrix(movie_UI, movie_sim)
-save(movie_pred, file = "movie_pred.RData")
