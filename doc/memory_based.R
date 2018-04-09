176cfbdb633012907cf9770f801573bf4bf32378
Juho Ma <35592552+juhoma@users.noreply.github.com>
2018-04-08 23:43:36 -0400

    Update memory_based.R

--- doc/memory_based.R
+++ doc/memory_based.R
@@ -13,19 +13,20 @@ library(infotheo)
 library(boot)
 
 ### Set path for functions.R
-
 setwd("~/STAT4243/project-3-algorithms-project3_group1/lib")
 source("functions.R")
 
-#### Predict MS dataset
+### Predict MS dataset
 
-### Build UI Matrix
+## Build UI Matrix
 
 # Set path for MS dataset
 setwd("~/STAT4243/project-3-algorithms-project3_group1/data/MS_sample")
+
 # Load the data
 MS_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
 MS_train <- MS_train[, 2:4]
+
 # Transform into UI matrix using MS_data_transform function
 MS_UI <- MS_data_transform(MS_train)
 save(MS_UI, file = "MS_UI.RData")
@@ -57,9 +58,97 @@ save(MS_sim_msd, file= "MS_sim_msd.RData")
 MS_sim_simrank <- calc_weight(MS_UI, method = "simrank")
 save(MS_sim_simrank, file= "MS_sim_simrank.RData")
 
-# Calculate predictions for MS
-MS_pred <- pred_matrix(MS_UI, MS_sim)
-save(MS_pred, file = "MS_pred.RData")
+# Calculate predictions for MS using each measure
+MS_pred_pearson <- pred_matrix(MS_UI, MS_sim_pearson)
+save(MS_pred_pearson, file = "MS_pred_pearson.RData")
+
+MS_pred_psig <- pred_matrix(MS_UI, MS_sim_psig)
+save(MS_pred_psig, file = "MS_pred_psig.RData")
+
+MS_pred_pvar <- pred_matrix(MS_UI, MS_sim_pvar)
+save(MS_pred_pvar, file = "MS_pred_pvar.RData")
+
+MS_pred_spearman <- pred_matrix(MS_UI, MS_sim_spearman)
+save(MS_pred_spearman, file = "MS_pred_spearman.RData")
+
+MS_pred_vector <- pred_matrix(MS_UI, MS_sim_vector)
+save(MS_pred_vector, file = "MS_pred_vector.RData")
+
+MS_pred_entropy <- pred_matrix(MS_UI, MS_sim_entropy)
+save(MS_pred_entropy, file = "MS_pred_entropy.RData")
+
+MS_pred_msd <- pred_matrix(MS_UI, MS_sim_msd)
+save(MS_pred_pearson, file = "MS_pred_pearson.RData")
+
+MS_pred_simrank <- pred_matrix(MS_UI, MS_sim_simrank)
+save(MS_pred_simrank, file = "MS_pred_simrank.RData")
+
+### Predict movie dataset
+
+## Build UI Matrix
+
+# Set path for movie dataset
+setwd("~/STAT4243/project-3-algorithms-project3_group1/data/eachmovie_sample")
+
+# Load the data
+MS_train <- read.csv("data_train.csv", as.is = TRUE, header = TRUE)
+MS_train <- MS_train[, 2:4]
+
+# Transform into UI matrix using MS_data_transform function
+MS_UI <- MS_data_transform(MS_train)
+save(MS_UI, file = "MS_UI.RData")
+
+# Calculate similarity matrices using each measure
+setwd("~/STAT4243/project-3-algorithms-project3_group1/output")
+
+MS_sim_pearson <- calc_weight(MS_UI, method = "pearson")
+save(MS_sim_pearson, file= "MS_sim_pearson.RData")
+
+MS_sim_psig <- calc_weight(MS_UI, method = "psig")
+save(MS_sim_psig, file= "MS_sim_psig.RData")
+
+MS_sim_pvar <- calc_weight(MS_UI, method = "pvar")
+save(MS_sim_pvar, file= "MS_sim_pvar.RData")
+
+MS_sim_spearman <- calc_weight(MS_UI, method = "spearman")
+save(MS_sim_spearman, file= "MS_sim_spearman.RData")
+
+MS_sim_vector <- calc_weight(MS_UI, method = "vector")
+save(MS_sim_vector, file= "MS_sim_vector.RData")
+
+MS_sim_entropy <- calc_weight(MS_UI, method = "entropy")
+save(MS_sim_entropy, file= "MS_sim_entropy.RData")
+
+MS_sim_msd <- calc_weight(MS_UI, method = "msd")
+save(MS_sim_msd, file= "MS_sim_msd.RData")
+
+MS_sim_simrank <- calc_weight(MS_UI, method = "simrank")
+save(MS_sim_simrank, file= "MS_sim_simrank.RData")
+
+# Calculate predictions for MS using each measure
+MS_pred_pearson <- pred_matrix(MS_UI, MS_sim_pearson)
+save(MS_pred_pearson, file = "MS_pred_pearson.RData")
+
+MS_pred_psig <- pred_matrix(MS_UI, MS_sim_psig)
+save(MS_pred_psig, file = "MS_pred_psig.RData")
+
+MS_pred_pvar <- pred_matrix(MS_UI, MS_sim_pvar)
+save(MS_pred_pvar, file = "MS_pred_pvar.RData")
+
+MS_pred_spearman <- pred_matrix(MS_UI, MS_sim_spearman)
+save(MS_pred_spearman, file = "MS_pred_spearman.RData")
+
+MS_pred_vector <- pred_matrix(MS_UI, MS_sim_vector)
+save(MS_pred_vector, file = "MS_pred_vector.RData")
+
+MS_pred_entropy <- pred_matrix(MS_UI, MS_sim_entropy)
+save(MS_pred_entropy, file = "MS_pred_entropy.RData")
+
+MS_pred_msd <- pred_matrix(MS_UI, MS_sim_msd)
+save(MS_pred_pearson, file = "MS_pred_pearson.RData")
+
+MS_pred_simrank <- pred_matrix(MS_UI, MS_sim_simrank)
+save(MS_pred_simrank, file = "MS_pred_simrank.RData")
 
 
 
